8fa2b95882ec5c14c675ebf019ad8cc2a558721a
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-08 11:57:49 -0300

    primeiro commit

---
+++ VETOR_NÚMERO NEGATIVO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VETOR_NÚMERO_NEGATIVO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet;
+
+            Console.WriteLine("Entre com os números: ");
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+            Console.WriteLine(); 
+            Console.WriteLine("Os números negativos são: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+
+            }
+        }
+    }
+}
